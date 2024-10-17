8f210e40a3273d441565043f2c8b65a6a5409794
nowcow <hhjnn92@icloud.com>
2024-10-18 04:52:53 +0900

    간단한 회원가입 로직

---
+++ src/main/java/com/example/jwt/controller/JoinController.java
@@ -0,0 +1,24 @@
+package com.example.jwt.controller;
+
+import com.example.jwt.dto.JoinDTO;
+import com.example.jwt.service.JoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+@RequiredArgsConstructor
+public class JoinController {
+
+    private final JoinService joinService;
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO){
+
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+}
